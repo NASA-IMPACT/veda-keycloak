a9c7cb68cbf418824b7c1c71e64f91370a3db6b9
Anthony Lukach <anthonylukach@gmail.com>
2024-10-17 11:00:59 -0700

    feat: Get configuration from environment

--- deploy/veda-keycloak.ts
+++ deploy/veda-keycloak.ts
@@ -3,6 +3,18 @@ import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import { KeycloakStack } from "./lib/KeycloakStack";
 import { join } from "path";
+import assert = require("assert");
+
+const {
+  AWS_ACCOUNT_ID,
+  AWS_DEFAULT_REGION,
+  SSL_CERTIFICATE_ARN,
+  HOSTNAME,
+  KEYCLOAK_VERSION = "26.0.0",
+} = process.env;
+
+assert(SSL_CERTIFICATE_ARN, "SSL_CERTIFICATE_ARN env var is required");
+assert(HOSTNAME, "HOSTNAME env var is required");
 
 const app = new cdk.App();
 new KeycloakStack(app, `VedaKeycloakStack-${process.env.STAGE || "dev"}`, {
@@ -14,11 +26,10 @@ new KeycloakStack(app, `VedaKeycloakStack-${process.env.STAGE || "dev"}`, {
   // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  env: { account: "853558080719", region: "us-west-2" },
+  env: { account: AWS_ACCOUNT_ID, region: AWS_DEFAULT_REGION },
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-  sslCertificateArn:
-    "arn:aws:acm:us-west-2:853558080719:certificate/012bf180-dc4c-4dee-9cba-d3eb64607e13",
-  hostname: "https://keycloak.delta-backend.xyz",
-  keycloakVersion: "26.0.0",
+  sslCertificateArn: SSL_CERTIFICATE_ARN,
+  hostname: HOSTNAME,
+  keycloakVersion: KEYCLOAK_VERSION,
   configDir: join(__dirname, "..", "config"),
 });
