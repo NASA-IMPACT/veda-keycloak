407adf744bb391338d8357c51ae5e72af1cb69a4
Anthony Lukach <anthonylukach@gmail.com>
2024-10-30 17:33:41 -0700

    Rename stack for better project conformance

--- deploy/app.ts
+++ deploy/app.ts
@@ -42,7 +42,7 @@ privateOauthClients.length
   : console.warn(`No client IDs found in ${CONFIG_DIR}`);
 
 const app = new cdk.App();
-new KeycloakStack(app, `VedaKeycloakStack-${STAGE}`, {
+new KeycloakStack(app, `veda-keycloak-${STAGE}`, {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
