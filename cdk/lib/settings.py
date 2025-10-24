56775757371869b4b13c763b33faf6283fa93ea1
smohiudd <saadiq@developmentseed.org>
2025-10-24 13:42:13 -0600

    fix settings

--- cdk/lib/settings.py
+++ cdk/lib/settings.py
@@ -21,7 +21,7 @@ class Settings(BaseSettings):
         pattern=r"^arn:aws:rds:[a-z0-9-]+:\d{12}:snapshot:.+$",
     )
     configure_route53: Optional[bool] = True
-    keycloak_send_email_address: Optional[str] = None
+    keycloak_send_email_address: Optional[str] = ""
 
     @field_validator("rds_snapshot_identifier", mode="before")
     @classmethod
